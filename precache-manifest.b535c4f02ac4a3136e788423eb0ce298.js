d76f6cecca4ac31903c608a9494571226d77f6c1
root <root@Pearl.localdomain>
2019-01-29 12:18:16 -0800

    Updates

---
+++ precache-manifest.b535c4f02ac4a3136e788423eb0ce298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60359adbf7f1403c4088",
+    "url": "/ge-price-checker/static/css/main.e18248bf.chunk.css"
+  },
+  {
+    "revision": "60359adbf7f1403c4088",
+    "url": "/ge-price-checker/static/js/main.60359adb.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/ge-price-checker/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "96e99a03eb2ae5a533a9",
+    "url": "/ge-price-checker/static/js/runtime~main.96e99a03.js"
+  },
+  {
+    "revision": "4cafd82c46af585ef506b746b39b2804",
+    "url": "/ge-price-checker/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ge-price-checker/precache-manifest.b535c4f02ac4a3136e788423eb0ce298.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ge-price-checker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
